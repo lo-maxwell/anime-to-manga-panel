0ceb7f8095053b682a7eeccda247a735e73271f2
unknown <leminhyen2@gmail.com>
2022-06-10 11:28:05 +0700

    add updates

--- convert_folder.py
+++ convert_folder.py
@@ -4,35 +4,36 @@ import os
 import sys
 #Want to get similar colors to manga page, not just grayscale
 
+def checkIfDirectoryExistsIfNotCreateOne(nameOfDirectory):
+    if not (os.path.exists(nameOfDirectory) and os.path.isdir(nameOfDirectory)):
+        os.makedirs(nameOfDirectory)
+
 def main(args):
-    conversionType = args[0] if len(args) > 0 else 'ERROR'
+    conversionType = args[0].lower() if len(args) > 0 else 'ERROR'
     originalDir = args[1] if len(args) > 1 else 'pages/mainstream'
     newDir = args[2] if len(args) > 2 else 'pages/mainstream_gray'
     value = max(int(args[3]), 1) if len(args) > 3 else 1
 
-    def checkIfDirectoryExistsIfNotCreateOne(nameOfDirectory):
-        if not (os.path.exists(nameOfDirectory) and os.path.isdir(nameOfDirectory)):
-            os.makedirs(nameOfDirectory)
-    
     checkIfDirectoryExistsIfNotCreateOne(newDir)
 
-    pages = [f for f in os.listdir(originalDir) if f.lower().endswith(('jpg', '.jpeg'))]
-    for p in pages:
-        originalPath = os.path.join(originalDir, p)
-        newPath = os.path.join(newDir, p)
-        if (conversionType.lower() == 'gray'):
+    listOfPages = [imageFileName for imageFileName in os.listdir(originalDir) if imageFileName.lower().endswith(('jpg', '.jpeg'))]
+
+    for page in listOfPages:
+        originalPath = os.path.join(originalDir, page)
+        newPath = os.path.join(newDir, page)
+        if (conversionType == 'gray'):
             custom_grayscale(originalPath, newPath, value) #2.98s
-        elif (conversionType.lower() == 'standard_gray'):
+        elif (conversionType == 'standard_gray'):
             builtin_grayscale(originalPath, newPath) #0.77s
-        elif (conversionType.lower() == 'hsv'):
+        elif (conversionType == 'hsv'):
             custom_hsv(originalPath, newPath)
-        elif (conversionType.lower() == 'standard_hsv'):
+        elif (conversionType == 'standard_hsv'):
             builtin_hsv(originalPath, newPath)
-        elif (conversionType.lower() == 'detail' or
-            conversionType.lower() == 'edge_enhance' or
-            conversionType.lower() == 'sharpen' or
-            conversionType.lower() == 'smooth'):
-            enhance(originalPath, newPath, conversionType.lower(), value)
+        elif (conversionType == 'detail' or
+            conversionType == 'edge_enhance' or
+            conversionType == 'sharpen' or
+            conversionType == 'smooth'):
+            enhance(originalPath, newPath, conversionType, value)
         else:
             print('Instruction format ' + conversionType + ' not recognized. See README for format types.')
             break
