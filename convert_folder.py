cf648a073b80df24051dc005d6829c256531dbb2
Maxwell Lo <lo-maxwell@berkeley.edu>
2022-06-09 18:14:22 -0700

    improving code readability

--- convert_folder.py
+++ convert_folder.py
@@ -7,7 +7,7 @@ def main(args):
     originalDir = args[0] if len(args) > 0 else 'pages/mainstream'
     newDir = args[1] if len(args) > 1 else 'pages/mainstream_gray'
     conversionType = args[2] if len(args) > 2 else 'all'
-    cluster = max(int(args[3]), 1) if len(args) > 3 else 5
+    value = max(int(args[3]), 1) if len(args) > 3 else 5
 
     if not (os.path.exists(newDir) and os.path.isdir(newDir)):
         os.makedirs(newDir)
@@ -17,19 +17,17 @@ def main(args):
         originalPath = os.path.join(originalDir, p)
         newPath = os.path.join(newDir, p)
         if (conversionType.lower() == 'gray'):
-            convert_cluster(originalPath, newPath, cluster) #2.98s
+            custom_grayscale(originalPath, newPath, value) #2.98s
         elif (conversionType.lower() == 'standard'):
-            convert_all(originalPath, newPath) #0.77s
-        elif (conversionType.lower() == 'define'):
-            define_borders(originalPath, newPath)
+            builtin_grayscale(originalPath, newPath) #0.77s
         elif (conversionType.lower() == 'detail' or
             conversionType.lower() == 'edge_enhance' or
             conversionType.lower() == 'sharpen' or
             conversionType.lower() == 'smooth'):
-            enhance(originalPath, newPath, conversionType.lower(), cluster)
+            enhance(originalPath, newPath, conversionType.lower(), value)
         else:
             print('Instruction format ' + conversionType + ' not recognized. See README for format types.')
             break
-        
+
 if __name__ == '__main__':
     main(sys.argv[1:])
--- current_best_convert_one.py
+++ current_best_convert_one.py
@@ -1,43 +1,26 @@
 #input: a single image from an anime in jpg/jpeg
 #output: a single image grayscaled in jpg/jpeg
 
-
 import cv2
 import time
 import os
 import sys
 from PIL import Image, ImageOps, ImageFilter
 
-#input: RGB of type int
-#output: RGB shifted to be more mangalike
-def grayscaleShift(color):
-    gray = color
-    if gray < 35:
-        gray = 0
-    if gray > 150:
-        gray = 255
-    if gray > 0:
-        gray = min(int((gray)*1.1), 255)
-    return gray
-
 #Input: RGB of type tuple
 #Output: RGB converted to grayscale of type tuple
-def colorToGrayscale(color, shift = False, cluster=1):
+def colorToGrayscale(color, cluster=1):
     #get gray color
     gray = 0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]
     #round to nearest cluster
     gray = int((gray//cluster)*cluster)
-
-    #shift for manga coloration
-    if shift:
-        gray = grayscaleShift(gray)
     return (gray, gray, gray)
 
-def increaseContrast(color, cluster=1):
-    if (color[0] + color[1] > 380\
-     or color[0] + color[2] > 380\
-     or color[1] + color[2] > 380\
-     or color[0] + color[1] + color[2] > 510):
+def increaseContrast(color):
+    if (    color[0] + color[1] > 380\
+         or color[0] + color[2] > 380\
+         or color[1] + color[2] > 380\
+         or color[0] + color[1] + color[2] > 510):
         color = (255,255,255)
     if (color[0] + color[1] + color[2] < 100):
         color = (0,0,0)
@@ -50,45 +33,12 @@ def colorShift(color, upThreshold, upPercent, upFlat, downThreshold, downPercent
         color = (min(int((color[0])*upPercent + upFlat), 255), min(int((color[1])*upPercent + upFlat), 255), min(int((color[2])*upPercent + upFlat), 255))
     if (colorSum <= downThreshold):
         color = (max(int((color[0])*downPercent - downFlat), 0), max(int((color[1])*downPercent - downFlat), 0), max(int((color[2])*downPercent - downFlat), 0))
-    # if upThreshold > downThreshold:
-    #     mid = downThreshold + (upThreshold - downThreshold)/2
-    #     if (colorSum <= mid):
-    #         shiftPercent = (2+min((mid - colorSum)/mid, 1)*downPercent)/3
-    #         color = (max(int((color[0])*shiftPercent), 0), max(int((color[1])*shiftPercent), 0), max(int((color[2])*shiftPercent), 0))
-    #     else:
-    #         shiftPercent = (2+max((colorSum - mid)/mid, 1)*upPercent)/3
-    #         color = (min(int((color[0])*shiftPercent), 255),min(int((color[1])*shiftPercent), 255), min(int((color[2])*shiftPercent), 255))
     return color
 
-def convert_all(originalPath, newPath):
+def builtin_grayscale(originalPath, newPath):
     image = Image.open(originalPath, 'r')
     gray_image = ImageOps.grayscale(image)
     gray_image.save(newPath)
-
-def setRCToBlack(matrix, row, col):
-    matrix[row*col + col] = (0,0,0)
-
-def setSurrounding(originalPixels, element, rows, cols):
-    row = element//rows
-    col = element%rows
-    for i in range(-1, 1):
-        for j in range(-1, 1):
-            if (row + i == -1 or row + i == rows + 1 or col + j == -1 or col + j == cols + 1):
-                continue
-            setRCToBlack(originalPixels, row + i, col + j)
-
-def define_borders(originalPath, newPath):
-    image = Image.open(originalPath, 'r')
-    pixels = list(image.getdata())
-    width, height = image.size
-    for i in range(len(pixels)):
-        if isinstance(pixels[i], int):
-            pixels[i] = (pixels[i], pixels[i], pixels[i])
-        if pixels[i] == (0,0,0):
-            setSurrounding(pixels, i, height, width)
-    new_image = Image.new(image.mode, image.size)
-    new_image.putdata(pixels)
-    new_image.save(newPath)
 
 def convert_cluster(originalPath, newPath, cluster=1):
     image = Image.open(originalPath, 'r')
@@ -96,8 +46,8 @@ def convert_cluster(originalPath, newPath, cluster=1):
     for i in range(len(pixels)):
         if isinstance(pixels[i], int):
             pixels[i] = (pixels[i], pixels[i], pixels[i])
-        pixels[i] = increaseContrast(pixels[i], cluster)
-        pixels[i] = colorToGrayscale(pixels[i], False, cluster)
+        pixels[i] = increaseContrast(pixels[i])
+        pixels[i] = colorToGrayscale(pixels[i], cluster)
     gray_image = Image.new(image.mode, image.size)
     gray_image.putdata(pixels)
     gray_image.save(newPath)
